35dbdfa77ff4146981434185bf7286a35bfb35ef
jiwon <kiwijomn@gmail.com>
2024-10-08 09:37:33 +0900

    🎉 Initial commit

---
+++ investment/src/test/java/codeping/flex/investment/InvestmentApplicationTests.java
@@ -0,0 +1,13 @@
+package codeping.flex.investment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
