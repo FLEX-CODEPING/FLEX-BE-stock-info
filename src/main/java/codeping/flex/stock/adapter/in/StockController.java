48d58755223909be108ccff99a9d854b3621ff03
joowojr <joowojr@gmail.com>
2024-11-16 16:39:43 +0900

    FLEX-225 :memo: Docs: 스웨거 명세 추가
    <footer>
    - #2

--- src/main/java/codeping/flex/stock/adapter/in/StockController.java
+++ src/main/java/codeping/flex/stock/adapter/in/StockController.java
@@ -23,14 +23,14 @@ public class StockController {
     private final StockInfoUsecase stockUsecase;
 
     @GetMapping("/{stockcode}")
-    @Operation(summary = "주식 정보를 조회합니다.", description = "회사 이름, 코드, 이미지, 관심 종목 여부 등을 반환합니다.")
+    @Operation(summary = "주식 정보를 조회합니다.", description = "회사 이름, 코드, 이미지, 관심 종목 여부 등을 반환합니다. 전일 변동률은 장전 시간 주식 ohlcv 조회에서 사용해주세요.")
     @ApiErrorCodes(stockErrors = {StockErrorCode.STOCK_NOT_FOUND})
     public ApplicationResponse<GetStockSummaryInfoDto> getStockSummaryInfo(@PathVariable String stockcode) {
         return ApplicationResponse.onSuccess(stockUsecase.getStockInfo(stockcode));
     }
 
     @GetMapping("/{stockcode}/ohlcv/preOpen")
-    @Operation(summary = "장전 시간 주식 시가/고가/저가/종가 조회", description = "장전 시간(오전 12~9시) 전일 주식 시가, 고가, 저가, 종가, 거래량,")
+    @Operation(summary = "장전 시간 주식 ohlcv 조회", description = "장전 시간(오전 12~9시) 전일 주식 시가, 고가, 저가, 종가, 거래량,")
     @ApiErrorCodes(stockErrors = {StockErrorCode.STOCK_OHLCV_NOT_FOUND})
     public ApplicationResponse<GetStockOHLCVInfoDto> getStockOHLCVInfo(@PathVariable String stockcode) {
         return ApplicationResponse.onSuccess(stockUsecase.getStockOHLCVInfo(stockcode));
