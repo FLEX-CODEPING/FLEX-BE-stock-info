15974d3146946a1b56f1f87be99b3cf938b6a667
joowojr <joowojr@gmail.com>
2024-11-19 11:19:00 +0900

    FLEX-249 :feature: Feat: JPA config 추가
    <footer>
    - 관련 : #13

---
+++ src/main/java/codeping/flex/stock/global/config/jpa/JpaAuditingConfig.java
@@ -0,0 +1,10 @@
+package codeping.flex.stock.global.config.jpa;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+
+}
