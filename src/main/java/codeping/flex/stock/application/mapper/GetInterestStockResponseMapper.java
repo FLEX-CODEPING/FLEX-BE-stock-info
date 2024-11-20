9844db723450d4daf0f71029be36732f018ea3db
joowojr <joowojr@gmail.com>
2024-11-20 19:28:51 +0900

    FLEX-255 :sparkles: Feat: EntityMapper 인터페이스 구현
    <footer>
    - 관련 : #15

---
+++ src/main/java/codeping/flex/stock/application/mapper/GetInterestStockResponseMapper.java
@@ -0,0 +1,40 @@
+package codeping.flex.stock.application.mapper;
+
+import codeping.flex.stock.application.port.in.dto.GetInterestStockInfoDto;
+import codeping.flex.stock.domain.InterestStock;
+import codeping.flex.stock.domain.StockImage;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import org.mapstruct.Named;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@Mapper(componentModel = "spring")
+public interface GetInterestStockResponseMapper {
+
+    @Mappings({
+            @Mapping(target = "id", source = "interestStock.id"),
+            @Mapping(target = "stockcode", source = "interestStock.stockcode"),
+            @Mapping(target = "corpName", source = "interestStock.corpName"),
+            @Mapping(target = "symbolImageUrl", source = "stockImage", qualifiedByName = "toSymbolImageUrl")
+    })
+    GetInterestStockInfoDto toDto(InterestStock interestStock, StockImage stockImage);
+
+    default List<GetInterestStockInfoDto> toDtoList(List<InterestStock> interestStocks, List<StockImage> stockImages) {
+        Map<String, StockImage> stockImageMap = stockImages.stream()
+                .collect(Collectors.toMap(StockImage::getStockcode, Function.identity(), (existing, replacement) -> existing));
+
+        return interestStocks.stream()
+                .map(interestStock -> toDto(interestStock, stockImageMap.get(interestStock.getStockcode())))
+                .collect(Collectors.toList());
+    }
+
+    @Named("toSymbolImageUrl")
+    default String toSymbolImageUrl(StockImage stockImage) {
+        return stockImage != null ? stockImage.getImageUrl() : null;
+    }
+}
