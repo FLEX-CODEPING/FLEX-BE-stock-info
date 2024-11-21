e33c3d6fc06c3661f7c62dff0850106b808a6184
joowojr <joowojr@gmail.com>
2024-11-21 14:11:58 +0900

    FLEX-262 :sparkles: Feat: 장 외 시간 주식 정보 조회 usecase 구현
    <footer>
    - 관련 : #17

--- src/main/java/codeping/flex/stock/application/port/in/StockInfoUsecase.java
+++ src/main/java/codeping/flex/stock/application/port/in/StockInfoUsecase.java
@@ -1,12 +1,13 @@
 package codeping.flex.stock.application.port.in;
 
 import codeping.flex.stock.application.port.in.dto.GetStockPreMarketInfoDto;
+import codeping.flex.stock.application.port.in.dto.GetStockPreOpenSummaryInfoDto;
 import codeping.flex.stock.application.port.in.dto.GetStockSummaryInfoDto;
 
 import java.time.LocalDate;
 
 public interface StockInfoUsecase {
     GetStockSummaryInfoDto getStockSummaryInfo(String stockcode);
-
+    GetStockPreOpenSummaryInfoDto getStockPreOpenSummaryInfo(String stockcode, LocalDate date);
     GetStockPreMarketInfoDto getStockPreMarketInfo(String stockcode, LocalDate date);
 }
