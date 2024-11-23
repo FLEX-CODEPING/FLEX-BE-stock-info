8a6fb40fec2d5fdc16c4a043db598521c887e9a2
joowojr <joowojr@gmail.com>
2024-11-23 14:53:10 +0900

    FLEX-267 :recycle: Refactor: search type으로 분기 처리 검색 / 문자열이 숫자인지의 조건 분기 처리 검색으로 리팩토링
    <footer>
    - 관련 : #21

--- src/main/java/codeping/flex/stock/adapter/in/StockSearchController.java
+++ src/main/java/codeping/flex/stock/adapter/in/StockSearchController.java
@@ -3,7 +3,9 @@ package codeping.flex.stock.adapter.in;
 import codeping.flex.stock.application.StockSearchService;
 import codeping.flex.stock.application.port.in.dto.GetStockAutoCompleteDto;
 import codeping.flex.stock.global.annotation.architecture.WebAdapter;
+import codeping.flex.stock.global.common.exception.ApplicationException;
 import codeping.flex.stock.global.common.response.ApplicationResponse;
+import codeping.flex.stock.global.common.response.code.CommonErrorCode;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -24,17 +26,21 @@ import java.util.List;
 public class StockSearchController {
     private final StockSearchService stockSearchService;
 
-    @GetMapping("/autocomplete/corpName")
-    @Operation(summary = "기업명 자동 완성 검색", description = "기업명 자동 완성 검색결과를 조회합니다. 접두어를 기준으로 검색한 결과를 반환합니다.")
-    public ApplicationResponse<List<GetStockAutoCompleteDto>> getStocksByKeyword(@Parameter(example = "삼성") @RequestParam("keyword") String keyword,
-                                                                                 @RequestParam("size") int size) throws IOException {
-        return ApplicationResponse.onSuccess(stockSearchService.autoCompleteCorpName(keyword, size));
+    @GetMapping("/autoComplete/V1")
+    @Operation(summary = "자동 완성 검색", description = "자동 완성 검색결과를 조회합니다. 접두어를 기준으로 검색하며, 문자열의 숫자/문자 여부에 따라 결과를 반환합니다.")
+    public ApplicationResponse<List<GetStockAutoCompleteDto>> getAutoCompleteStocks(@Parameter(example = "삼성") @RequestParam("keyword") String keyword,
+                                                                                    @RequestParam("size") int size) throws IOException {
+        return ApplicationResponse.onSuccess(stockSearchService.getAutoCompleteStocks(keyword, size));
     }
 
-    @GetMapping("/autocomplete/stockCode")
-    @Operation(summary = "기업명 자동 완성 검색", description = "종목 코드 자동 완성 검색결과를 조회합니다. 접두어를 기준으로 검색한 결과를 반환합니다.")
-    public ApplicationResponse<List<GetStockAutoCompleteDto>> getStocksByKeywords(@Parameter(example = "0000") @RequestParam("keyword") String keyword,
-                                                                                  @RequestParam("size") int size) throws IOException {
-        return ApplicationResponse.onSuccess(stockSearchService.autoCompleteStockcode(keyword, size));
+    @GetMapping("/autoComplete/V2")
+    @Operation(summary = "자동 완성 검색", description = "자동 완성 검색결과를 조회합니다. 접두어를 기준으로 검색한 결과를 반환합니다.")
+    public ApplicationResponse<List<GetStockAutoCompleteDto>> getStocksByKeyword(@Parameter(example = "corpName", description = "stockcode 또는 corpName 입력") @RequestParam("searchType") String searchType,
+                                                                                 @Parameter(example = "삼성") @RequestParam("keyword") String keyword,
+                                                                                 @RequestParam("size") int size) throws IOException {
+        if(!searchType.equals("stockcode") && !searchType.equals("corpName")) {
+            throw ApplicationException.from(CommonErrorCode.BAD_REQUEST);
+        }
+        return ApplicationResponse.onSuccess(stockSearchService.getAutoCompleteStocks(keyword, size));
     }
 }
