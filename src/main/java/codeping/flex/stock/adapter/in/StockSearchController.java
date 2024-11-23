35d9f0911a7dec326bf80685a346090ca517e8a3
joowojr <joowojr@gmail.com>
2024-11-23 14:17:57 +0900

    FLEX-267 :memo: Docs: swagger 명세 추가
    <footer>
    - 관련 : #21

--- src/main/java/codeping/flex/stock/adapter/in/StockSearchController.java
+++ src/main/java/codeping/flex/stock/adapter/in/StockSearchController.java
@@ -4,7 +4,8 @@ import codeping.flex.stock.application.StockSearchService;
 import codeping.flex.stock.application.port.in.dto.GetStockAutoCompleteDto;
 import codeping.flex.stock.global.annotation.architecture.WebAdapter;
 import codeping.flex.stock.global.common.response.ApplicationResponse;
-import codeping.flex.stock.infrastructure.elasticsearch.document.StockDocument;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,13 +25,15 @@ public class StockSearchController {
     private final StockSearchService stockSearchService;
 
     @GetMapping("/autocomplete/corpName")
-    public ApplicationResponse<List<GetStockAutoCompleteDto>> getStocksByKeyword(@RequestParam("keyword") String keyword,
+    @Operation(summary = "기업명 자동 완성 검색", description = "기업명 자동 완성 검색결과를 조회합니다. 접두어를 기준으로 검색한 결과를 반환합니다.")
+    public ApplicationResponse<List<GetStockAutoCompleteDto>> getStocksByKeyword(@Parameter(example = "삼성") @RequestParam("keyword") String keyword,
                                                                                  @RequestParam("size") int size) throws IOException {
         return ApplicationResponse.onSuccess(stockSearchService.autoCompleteCorpName(keyword, size));
     }
 
     @GetMapping("/autocomplete/stockCode")
-    public ApplicationResponse<List<GetStockAutoCompleteDto>> getStocksByKeywords(@RequestParam("keyword") String keyword,
+    @Operation(summary = "기업명 자동 완성 검색", description = "종목 코드 자동 완성 검색결과를 조회합니다. 접두어를 기준으로 검색한 결과를 반환합니다.")
+    public ApplicationResponse<List<GetStockAutoCompleteDto>> getStocksByKeywords(@Parameter(example = "0000") @RequestParam("keyword") String keyword,
                                                                                   @RequestParam("size") int size) throws IOException {
         return ApplicationResponse.onSuccess(stockSearchService.autoCompleteStockcode(keyword, size));
     }
