fe8b2d57c7ffcd6c985f1b921f2ad9c77ec064cf
joowojr <joowojr@gmail.com>
2024-11-24 13:50:49 +0900

    FLEX-273 :sparkles: Feature: CorpInfo adapter layer 구현
    <footer>
    - 관련 : #23

---
+++ src/main/java/codeping/flex/stock/adapter/out/persistence/mapper/CorpInfoMapper.java
@@ -0,0 +1,12 @@
+package codeping.flex.stock.adapter.out.persistence.mapper;
+
+import codeping.flex.stock.adapter.out.persistence.entity.CorpInfoEntity;
+import codeping.flex.stock.adapter.out.persistence.mapper.common.ReadOnlyEntityMapper;
+import codeping.flex.stock.domain.CorpInfo;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public interface CorpInfoMapper extends ReadOnlyEntityMapper<CorpInfoEntity, CorpInfo> {
+    CorpInfoMapper INSTANCE = Mappers.getMapper(CorpInfoMapper.class);
+}
