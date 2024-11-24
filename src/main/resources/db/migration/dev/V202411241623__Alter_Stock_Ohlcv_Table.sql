a2ef24bfc3da90c1b8d4bb01bc48f4eb2835bdd5
joowojr <joowojr@gmail.com>
2024-11-24 21:24:56 +0900

    FLEX-274 :sparkles: Feat: "ticker" 컬럼 및 필드명 변경
    <footer>
    - 관련 : #25

--- src/main/resources/db/migration/dev/V202411241623__Alter_Stock_Ohlcv_Table.sql
+++ src/main/resources/db/migration/dev/V202411241623__Alter_Stock_Ohlcv_Table.sql
@@ -1,14 +1,14 @@
--- 1. ticker 컬럼 추가
+-- 1. stock_id 컬럼 추가
 ALTER TABLE stock_ohlcv
-    ADD COLUMN ticker VARCHAR(50);
+    ADD COLUMN stock_id VARCHAR(50);
 
 -- 2. 외래 키 제약 조건 추가
 ALTER TABLE stock_ohlcv
-    ADD CONSTRAINT FOREIGN KEY (ticker) REFERENCES stock(stockcode);
+    ADD CONSTRAINT FOREIGN KEY (stock_id) REFERENCES stock(stockcode);
 
--- 3. ticker 업데이트: stockcode가 존재할 경우에만 업데이트
+-- 3. stock_id 업데이트: stockcode가 존재할 경우에만 업데이트
 UPDATE stock_ohlcv so
-SET so.ticker = (
+SET so.stock_id = (
     SELECT s.stockcode
     FROM stock s
     WHERE s.stockcode = so.stockcode
